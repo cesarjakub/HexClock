df4a83a6d802f1f77a730c9ff39cea5f3e388df5
cesarjakub <list>
2022-12-27 20:35:12 +0100

    hotovson

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function prepisCas(){
+
     const date = new Date();
     let pozadi = document.querySelector("body");
     let hours = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
-    let time = hours+""+min+""+sec;
+    
     if(hours < 10){
         hours = "0"+hours;
     }
@@ -14,7 +15,8 @@ function prepisCas(){
     if(sec < 10){
         sec = "0"+sec;
     }
-    pozadi.style.background = "#"+time;
+    let time = hours+""+min+""+sec;
+    pozadi.style.backgroundColor = "#"+time;
     document.querySelector(".Mian_Clock").innerHTML = "#"+time;
     setTimeout(prepisCas, 1000);
 }
