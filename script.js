ea34636911f4fe1963749035e09ef6629698d30a
cesarjakub <list>
2022-12-27 20:15:49 +0100

    skoro hotovo

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let mainClock = document.querySelector("#Mian_Clock");
+let mainClock = document.querySelector(".Mian_Clock");
 const date = new Date();
 let hours = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
+let pozadi = document.querySelector("body");
 
-let time = "#"+hours+""+min+""+sec;
 
-console.log(time);
+
+
+function prepisCas(){
+    let time = "#"+hours+""+min+""+sec;
+    if(hours < 10){
+        hours = "0"+hours;
+    }
+    if(min < 10){
+        min = "0"+min;
+    }
+    if(sec < 10){
+        sec = "0"+sec;
+    }
+    mainClock.innerHTML = time;
+    pozadi.style.background = time;
+    mainClock.innerHTML = time;
+    setTimeout(prepisCas, 1000);
+}
+prepisCas();
+
